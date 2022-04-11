ed060e2837a97a5f7789ea8dcc6aa41f79fa80ea
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-04-10 22:20:59 -0300

    heroku deploy

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(xss());
 app.use(cors());
 
 app.set("port", process.env.APP_PORT || 4000);
-app.set("host", process.env.APP_HOST || "localhost");
+app.set("host", process.env.APP_HOST || "127.0.0.1");
 app.use("/api/v1", routes);
 app.use("/", (req, res) => {
   return res.send("welcome to Smart buy");
